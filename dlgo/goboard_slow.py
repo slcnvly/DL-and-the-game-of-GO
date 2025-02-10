07f5b0e9c753ee850e044eeb17875a3b3692ab5b
slcnvly <goodwon01@gmail.com>
2025-02-10 18:22:23 +0900

    수정

--- dlgo/goboard_slow.py
+++ dlgo/goboard_slow.py
@@ -20,3 +20,37 @@ class Move(): #수 설정 : 돌 놓기, 차례 넘기기, 대국 포기
     @classmethod
     def resign(cls):
         return Move(is_resign=True)
+    
+
+
+
+class GoString(): #이음을 set으로 인코딩
+    def __init__(self, color, stones, liberties):
+        self.color = color
+        self.stones = set(stones)
+        self.liberties = set(liberties)
+
+    def remove_liberty(self, point):
+        self.liberties.remove(point)
+
+    def add_liberty(self, point):
+        self.liberties.add(point)
+
+    def merged_with(self, go_string): #양 선수의 이음의 모든 돌을 저장한 새 이음을 반환한다 ( 두 개의 그룹을 연결한 경우 merged_with()메소드 호출 )
+        assert go_string.color == self.color
+        combined_stones = self.stones | go_string.stones
+        return GoString(
+            self.color,
+            combined_stones,
+            (self.liberties | go_string.liberties) - combined_stones
+        )
+    
+    @property
+    def num_liberties(self):
+        return len(self.liberties)
+    def __eq__(self, other):
+        return isinstance(other, GoString) and \
+        self.color == other.color and \
+        self.stones == other.stones and \
+        self.liberties == other.liberties
+    
